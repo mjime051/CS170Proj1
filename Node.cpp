2160c0096ec55233cddd0802a26454c4cbda581d
mjime051 <43627981+mjime051@users.noreply.github.com>
2020-02-17 15:59:20 -0800

    Added functions
    Needed functions to determine where the blank space is. Will help when shuffling the 8-puzzle.

--- Node.cpp
+++ Node.cpp
@@ -14,6 +14,32 @@ Node* Node::getParent() {
 	return this->parent;
 }
 
+int Node::getCost() {
+	return this->cost;
+}
+
+int Node::getBlankX() {
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++) {
+			if (this->state[i][j] == 0) {
+				return i;
+			}
+		}
+	}
+}
+
+int Node::getBlankY() {
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++) {
+			if (this->state[i][j] == 0) {
+				return j;
+			}
+		}
+	}
+}
+
 void Node::printState() {
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
